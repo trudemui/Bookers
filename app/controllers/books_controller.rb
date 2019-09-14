4ad21e9cbaebc1dc4f97f86bd757604bbd4195a2
trudemui <alesth_130@yahoo.co.jp>
2019-09-14 19:45:16 +0900

    [add]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to books_path
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
